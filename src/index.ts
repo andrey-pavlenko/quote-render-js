996a468eebaab4986b0860702e706f41a35c41dd
Andrey Pavlenko <31999255+andrey-pavlenko@users.noreply.github.com>
2020-07-19 13:40:45 +0300

    Add simple templates

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.info('Hello');
+import pug from 'pug';
+import data from './templates/data.json';
+
+const simpleHtml = pug.compileFile('./src/templates/simple-html/index.pug', {
+  pretty: true
+});
+
+console.info(simpleHtml(data));
